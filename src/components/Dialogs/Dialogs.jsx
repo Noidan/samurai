09fdb9e1194d920ed64679a9160bed6f2d7e05a4
Tony <122675474+Noidan@users.noreply.github.com>
2023-07-18 15:04:02 +0300

    lessin21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import classes from './Dialogs.module.css'
 
 
 const Dialogs = (props) => {
-    return <div>
-        Dialogs
+    return <div className={classes.dialogs}>
+        <div className={classes.dialogsItems}>
+            <div className={classes.dialog + ' ' + classes.active}>
+                Tony Stark
+            </div>
+            <div className={classes.dialog}>
+                Steve Rogers
+            </div>
+            <div className={classes.dialog}>
+                Peter Benjamin Parker
+            </div>
+            <div className={classes.dialog}>
+                Dr. Robert Bruce Banner
+            </div>
+            <div className={classes.dialog}>
+                Natasha Romanoff
+            </div>
+        </div>
+        <div className={classes.messages} > 
+            <div className={classes.message}>Hi!</div>
+            <div className={classes.message}>How you doin?</div>
+            <div className={classes.message}>Hi...</div>
+        </div>
     </div>
 
 }
